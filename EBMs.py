0ae2ec89d4a340ffa00dfe2589e471dbf13cd854
Bamboohoccode <tretrinh2k7@gmail.com>
2026-09-02 08:18:44 +0700

    Finale fixing

--- EBMs.py
+++ EBMs.py
@@ -7,9 +7,10 @@ import numpy as np
 import argparse, os
 import math
 class EBMs(nn.Module):
-    def __init__(self,input_dims = 784,hidden_dims = 1):
+    def __init__(self,input_dims = 784,hidden_dims = 256):
         super().__init__()
         self.net = nn.Sequential(
+            nn.Flatten()
             nn.Linear(in_features=input_dims,
                     out_features=hidden_dims),
             nn.SiLU(),
@@ -37,18 +38,21 @@ class ReplayBuffer():
         else:
             return torch.rand((batch_size, 1, self.size, self.size), device=device) * 2.0 - 1.0
     def add(self,x):
-        if(len(self.buffer) < self.max_size):
-            self.buffer.append(x)
-        else:
-            self.buffer[torch.randint(0,self.max_size - 1,1).item()] = x
+        x_cpu = x.detach().cpu()
+        for img in x_cpu:
+            if len(self.buffer) < self.max_size:
+                self.buffer.append(img)
+            else:
+                idx = torch.randint(0, self.max_size, (1,)).item()
+                self.buffer[idx] = img
 
-def sample_sgld(model,x_init, num_steps=60, step_size=10.0, noise_std=0.005,alpha = 0.5):
+def sample_sgld(model,x_init, num_steps=60,alpha = 0.5):
     model.eval()
     x = x_init.clone().detach().requires_grad_(True)
 
     for step in range(num_steps):
 
-        grad = torch.autograd.grad(model(x),x,create_graph=False)
+        grad = torch.autograd.grad(model(x).sum(),x,create_graph=False)[0]
         noise = torch.randn_like(x)
         # using x.data for not tracking grad every different x.
         x.data = x.data - alpha * grad + torch.sqrt(2 * alpha) * noise
@@ -58,7 +62,7 @@ def sample_sgld(model,x_init, num_steps=60, step_size=10.0, noise_std=0.005,alph
 def get_dataloader(batch_size=128):
     transform = transforms.Compose([
         transforms.ToTensor(),
-        transforms.Normalize((0.5,), (0.5,)) # Chuyển về [-1, 1]
+        transforms.Normalize((0.5,), (0.5,))
     ])
     dataset = datasets.MNIST(root="./data", train=True, transform=transform, download=True)
     return DataLoader(dataset, batch_size=batch_size, shuffle=True, drop_last=True)
@@ -69,8 +73,9 @@ def train(model,cfg):
     Buffer = ReplayBuffer()
     optimizer = optim.AdamW(model.parameters(),lr = cfg['lr'])
     dataloader = get_dataloader()
+    os.makedirs("./samples", exist_ok=True)
     for epoch in range(cfg['epochs']):
-        for step,x_real in enumerate(dataloader):
+        for step,(x_real,_) in enumerate(dataloader):
             x_real = x_real.to(cfg['device'])
             batch_size = x_real.size(0)
 
@@ -102,7 +107,8 @@ if __name__ == "__main__":
     DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     parser = argparse.ArgumentParser()
     parser.add_argument("--lr",
-                        default=DEFAULT_LR)
+                        default=DEFAULT_LR,
+                        type = float)
     parser.add_argument("--epochs",
                         default=DEFAULT_EPOCHS,
                         type=int)
